5a88e099765ecf713654c343b790a588c77ee6f5
furkankarakuz <karakuzfurkan.98@gmail.com>
2025-03-12 02:10:22 +0300

    edit text_process

--- text_process.py
+++ text_process.py
@@ -10,8 +10,8 @@ stop_words = [stop_word.translate(turkish_map) for stop_word in stop_words]
 
 def clean_text(text):
     text = re.sub(r'[^\w\s]', '', text)
-    text = text.lower()
     text = text.translate(turkish_map)
+    text = text.lower()
     text = re.sub(r'\d+', '', text)
     text = ' '.join([word for word in text.split() if word not in stop_words])
     text = re.sub(r'\s+', ' ', text).strip()
